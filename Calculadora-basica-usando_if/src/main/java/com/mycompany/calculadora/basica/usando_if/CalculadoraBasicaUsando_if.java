b45863ebf5b8db9d62cd1bba593591b08a1636dd
markigameplay <80642392+markigameplay@users.noreply.github.com>
2024-09-10 17:36:29 -0500

    nuevo v2

---
+++ Calculadora-basica-usando_if/src/main/java/com/mycompany/calculadora/basica/usando_if/CalculadoraBasicaUsando_if.java
@@ -0,0 +1,27 @@
+package com.mycompany.calculadora.basica.usando_if;
+
+import java.util.Scanner;
+
+/**
+ * Objetivo Escribe un programa en Java que actúe como una calculadora básica.
+ * El programa solicitará al usuario ingresar dos números y una operación
+ * matemática (+, -, x, /). Luego, calculará la operación seleccionada y
+ * mostrará el resultado en la pantalla.
+ *
+ * Si el símbolo de la operación es diferente a los operadores permitidos (+, -,
+ * x, /), el programa indicará "Caracter no reconocido".
+ *
+ * @author pc
+ */
+public class CalculadoraBasicaUsando_if {
+
+    public static void main(String[] args) {
+        Scanner 
+        
+        
+        
+        
+        
+        
+    }
+}
