5f673bcc57b7d3af05661c2bd69d57928849d237
Mauricio Plaza <mauricioplaza0378@gmail.com>
2024-10-31 10:11:49 -0500

    melo

---
+++ Calculadora_basicacopy/src/main/java/com/mycompany/calculadora_basica/Calculadora_basicacopy.java
@@ -0,0 +1,30 @@
+
+
+package com.mycompany.calculadora_basicacopy;
+
+import javax.swing.JOptionPane;
+
+
+public class Calculadora_basica {
+
+    public static void main(String[] args) {
+       
+       int n = 2;
+       
+        switch (args) {
+        case 1:
+            System.out.println("el numero es 1");
+            break;
+       case 2: 
+       System.out.println("el numero es 2");
+       break;
+
+    
+        default:
+            System.out.println("EL numero no es 1 ni 2 ni 3");
+       }
+    
+    
+    
+    }
+}
---
+++ demo/src/main/java/com/example/Main.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        double promedio;
+        String condicionalFinal;
+        
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("ingrese los datos de la nota");
+        promedio =teclado.nextDouble();
+
+        condicionalFinal = promedio >= 6 ? "Aprobo" : "No aprobo";
+        System.out.println("la condicion final del alumno es: " condicionalFinal );
+
+
+
+    }
+}
